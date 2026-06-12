5aecd978cceaf09ca740733cd69a2e7f09165353
david32768 <81751257+david32768@users.noreply.github.com>
2026-06-12 19:37:18 +0100

    Add files via upload

---
+++ src/com/github/david32768/jynxto/tojynx/MainToJynx.java
@@ -0,0 +1,47 @@
+package com.github.david32768.jynxto.tojynx;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static com.github.david32768.jynxfree.jynx.Global.LOG;
+
+import com.github.david32768.jynxfree.jynx.ClassUtil;
+import com.github.david32768.jynxfree.jynx.MainOption;
+import com.github.david32768.jynxfree.jynx.MainOptionService;
+
+public class MainToJynx implements MainOptionService {
+
+    @Override
+    public MainOption main() {
+        return MainOption.DISASSEMBLY;
+    }
+    
+    @Override
+    public String version() {
+        return "0.25.4";
+    }
+
+    @Override
+    public boolean call(PrintWriter pw, String fname) {
+        byte[] bytes;
+        try {
+            bytes = ClassUtil.getClassBytes(fname);
+        } catch (IOException ex) {
+            LOG(ex);
+            return false;
+        }
+        return ToJynx.toJynx(bytes, pw);
+    }
+    
+
+    @Override
+    public String callToString(byte[] bytes) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        boolean success = ToJynx.toJynx(bytes, pw);
+        pw.flush();
+        return success? sw.toString(): null;
+    }
+
+}
